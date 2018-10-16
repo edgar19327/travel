39d3de7b12a44c4356697dd3a83bc6b760409b66
unknown <harutyunyanedgar82@gmail.com>
2018-10-16 15:42:34 +0400

    viewsTravel

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edgar
+ * Date: 15.10.2018
+ * Time: 23:18
+ */
---
+++ resources/views/placeSlider.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Edgar
+ * Date: 12.10.2018
+ * Time: 15:39
+ */
